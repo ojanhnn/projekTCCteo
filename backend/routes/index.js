b16df2a046012383a1742b40b932abf0fe508ef1
rozan <ojanhannan32@gmail.com>
2025-06-11 01:52:59 +0700

    ada yang baru

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,28 +1,29 @@
 import express from "express";
-import { getUsers, Register, Login, Logout} from "../controllers/UserController.js";
+import { getUsers, Register, Login, Logout } from "../controllers/UserController.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 import {
-    getData, 
-    getDataById, 
-    createData,
-    updateData,
-    deleteData
+    getKegiatan, 
+    getKegiatanById, 
+    createKegiatan,
+    updateKegiatan,
+    deleteKegiatan
 } from "../controllers/DataController.js";
 
 const router = express.Router();
 
+// Auth routes
 router.get('/user', verifyToken, getUsers);
 router.post('/user', Register);
 router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
 
-router.get("/data", verifyToken, getData);
-router.get("/data/:id", verifyToken, getDataById);
-router.post("/data", verifyToken, createData);
-router.patch("/data/:id", verifyToken, updateData);
-router.delete("/data/:id", verifyToken, deleteData);
-
+// Data routes
+router.get("/data", verifyToken, getKegiatan);
+router.get("/data/:id", verifyToken, getKegiatanById);
+router.post("/data", verifyToken, createKegiatan);
+router.patch("/data/:id", verifyToken, updateKegiatan);
+router.delete("/data/:id", verifyToken, deleteKegiatan);
 
 export default router;
